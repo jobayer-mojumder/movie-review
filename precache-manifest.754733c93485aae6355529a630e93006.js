e925de386a36eb0445d92588db1c10ea9fb6ba5e
jobayermojumder <jobayer.pro@gmail.com>
2020-07-16 13:02:59 +0200

    Updates

---
+++ precache-manifest.754733c93485aae6355529a630e93006.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50403878d50ac95e732fde5bd61d0e1a",
+    "url": "/movie-review/index.html"
+  },
+  {
+    "revision": "e5f898e5d51f49848e03",
+    "url": "/movie-review/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "efb3c2dcf19936dabb6f",
+    "url": "/movie-review/static/css/main.691204bf.chunk.css"
+  },
+  {
+    "revision": "e5f898e5d51f49848e03",
+    "url": "/movie-review/static/js/2.75784721.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movie-review/static/js/2.75784721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efb3c2dcf19936dabb6f",
+    "url": "/movie-review/static/js/main.bbd99ba9.chunk.js"
+  },
+  {
+    "revision": "24b64eea3cb883fa311b",
+    "url": "/movie-review/static/js/runtime-main.1f55eee0.js"
+  }
+]);
