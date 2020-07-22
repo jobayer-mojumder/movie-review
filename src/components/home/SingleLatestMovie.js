34351d4dd22b139954767f4fe9bc87ff12093e98
jobayermojumder <jobayer.pro@gmail.com>
2020-07-23 00:19:59 +0200

    flipper added

--- src/components/home/SingleLatestMovie.js
+++ src/components/home/SingleLatestMovie.js
@@ -7,17 +7,34 @@ export default class SingleLatestMovie extends Component {
         super(props)
     }
 
+    getYear(data) {
+        var d = new Date(data);
+        var n = d.getFullYear();
+        return n;
+    }
+
+    getShortOverview(data) {
+        if (data.length > 350) {
+            return data.substr(0, 350) + '...'
+        } else {
+            return data
+        }
+    }
+
     render() {
         return (
             <div className="col-sm-6 col-md-3">
                 <div className="latest-movie">
+                    <div className="flipper">
+                        <div className="front">
+                            <Link to="/"><img src={process.env.REACT_APP_IMAGE_PATH + this.props.movie.poster_path} alt="Movie 3" /></Link>
+                        </div>
 
-                    <div></div>
-
-                    <div class="back face">
-
+                        <div className="back">
+                            <h4>{this.props.movie.title} ({this.getYear(this.props.movie.release_date)})</h4>
+                            <p>{this.getShortOverview(this.props.movie.overview)}</p>
+                        </div>
                     </div>
-                    <Link to="/"><img src={process.env.REACT_APP_IMAGE_PATH + this.props.movie.poster_path} alt="Movie 3" /></Link>
                 </div>
             </div>
         )
